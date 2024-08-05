2f3f0018fb9c30088f250548b615c0e1830cd727
Akanksha kotwal <121031023+Akanksha880@users.noreply.github.com>
2024-08-05 21:59:58 +0530

    Update FILE word and its count.cpp

--- FILE word and its count.cpp
+++ FILE word and its count.cpp
@@ -1,79 +1,40 @@
 #include<iostream>
-
 #include<fstream>
-
 #include<string>
-
 #include<vector>
-
 using namespace std;
-
 int main()
-
 {
-
- ifstream my_file("file.txt");
-
+ifstream my_file("file.txt");
  if(!my_file)
-
- {
-
- cout<<"Error! Unable to open the file."<<endl;
-
- return 1;
-
- }
-
- //store the contents of the file in "line" string
-
- string line;
-
- int word_c=0;
-
- while(!my_file.eof())
-
+{
+cout<<"Error! Unable to open the file."<<endl;
+return 1;
+}
+//store the contents of the file in "line" string
+string line;
+int word_c=0;
+while(!my_file.eof())
  { //use eof() to check the end of data
-
- //store the current lin of the file in the "line" variable
-
- string word="";
-
- getline(my_file,line);
-
- for(auto i: line){
-
-  if(i==' ')
-
-  {
-
-  word_c++;
-
-  cout<<word<<endl;
-
-  word="";
-
-  }
-
-  else
-
-  {
-
-  word+=i;
-
-  }
-
- }
-
- }
-
+//store the current lin of the file in the "line" variable
+string word="";
+getline(my_file,line);
+for(auto i: line){
+if(i==' ')
+ {
+ word_c++;
+cout<<word<<endl;
+word="";
+}
+else
+{
+word+=i;
+}
+} }
  cout<<"count: "<<word_c<<endl;
-
- my_file.close(); //to prevent memory leakage or data loss
-
- cout<<"\nFile open status: "<<my_file.is_open()<<endl;
-
- return 0;
-
+my_file.close(); //to prevent memory leakage or data loss
+cout<<"\nFile open status: "<<my_file.is_open()<<endl;
+return 0;
 }
 
 
